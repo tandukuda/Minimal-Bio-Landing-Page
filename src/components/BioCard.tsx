18a3f8bd38b9ae95cd3aba9da2b5ed83fe0b4a9c
Ridho Ibrahim <133313292+tandukuda@users.noreply.github.com>
2025-04-30 00:14:04 +0700

    Update BioCard.tsx

--- src/components/BioCard.tsx
+++ src/components/BioCard.tsx
@@ -13,13 +13,13 @@ const BioCard: React.FC = () => {
         tandukuda
       </h1>
       <p style={{ marginBottom: '0.75rem' }}>
-        tandukuda is a 3D artist based in the bustling city of Surabaya, Indonesia, known for bringing
+        <b>tandukuda</b> is a 3D artist based in the bustling city of Surabaya, Indonesia, known for bringing
         hauntingly atmospheric visuals to life. From stage visuals and live productions to music
-        videos and NFTs, tandukuda's work spans diverse media, each piece immersing the audience in an
+        videos and NFTs, <b>tandukuda's</b> work spans diverse media, each piece immersing the audience in an
         unforgettable, eerie experience.
       </p>
       <p>
-        Driven by a relentless passion for innovation, tandukuda explores avant-garde techniques to
+        Driven by a relentless passion for innovation, <b>tandukuda</b> explores avant-garde techniques to
         create dark and captivating worlds that push the boundaries of digital design. With a unique
         style that merges horror and immersive storytelling, tandukuda's work is crafted to
         leave a lasting impact, engaging viewers and sparking inspiration in those who encounter his
